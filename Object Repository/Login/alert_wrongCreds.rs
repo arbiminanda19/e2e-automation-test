7a036504cdddeb3cc8c5bd92cea737ec5c701724
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-09-14 08:19:43 +0700

    implement DDT

---
+++ Object Repository/Login/alert_wrongCreds.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>alert_wrongCreds</name>
+   <tag></tag>
+   <elementGuidId>4b31c05e-c89b-46b1-a2b6-6f18dc63d7e3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@role='alert'][contains(text(),'Kredensial yang Anda berikan salah')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Test Suites/DDT for Login.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DDT for Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>528fcb50-ba8f-4e76-bd6f-9707dabfc69e</testSuiteGuid>
+   <testCaseLink>
+      <guid>a49fb8b6-8fb6-47b3-8e58-054573d1ba8f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web/DDT for Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>6e9c9af4-d78d-464b-8cc5-ddc36434caae</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>6e9c9af4-d78d-464b-8cc5-ddc36434caae</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>c380984e-d43b-4968-b336-e3db0fba5088</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>6e9c9af4-d78d-464b-8cc5-ddc36434caae</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>00e15840-34e5-43dd-afe4-ba98522db59a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>6e9c9af4-d78d-464b-8cc5-ddc36434caae</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>status</value>
+         <variableId>28fdd277-be2b-4d0a-abb1-df62c4e3ccb9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
